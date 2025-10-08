75b774e34be56d6a874e720ecc4c73e77b02f3cd
tinhbe <98801099+tvantinh@users.noreply.github.com>
2025-10-08 16:10:22 +0700

    Refactor JWT handling and enhance LoginResponse with default expiration time; implement refresh token management in JWTService

--- Zymora/Authentication/JWTHelper.cs
+++ Zymora/Authentication/JWTHelper.cs
@@ -5,44 +5,44 @@ using System.Security.Cryptography;
 using System.Text;
 namespace Zymora.Authentication
 {
-    public class JWTHelper
+  public class JWTHelper
+  {
+    public static string GenerateToken(string secretKey, IEnumerable<Claim>? claims, int expirationMinutes)
     {
-        public static string GenerateToken(string secretKey, IEnumerable<Claim>? claims, int expirationMinutes)
-        {
-            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
-            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            JwtSecurityToken token = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(expirationMinutes),
-                signingCredentials: creds
-            );
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
-        public static ClaimsPrincipal ValidateToken(string token, string secretKey)
-        {
-            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
-            byte[] key = Encoding.UTF8.GetBytes(secretKey);
-            TokenValidationParameters validationParameters = new TokenValidationParameters
-            {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(key),
-                ValidateLifetime = true,
-                ClockSkew = TimeSpan.Zero
-            };
-            try
-            {
-                return tokenHandler.ValidateToken(token, validationParameters, out _);
-            }
-            catch (Exception ex)
-            {
-                throw new SecurityTokenException("Invalid token", ex);
-            }
-        }
-        private string GenerateRefreshTokenAsync(string userId)
-        {
-          var refreshToken = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
-          return refreshToken;
-        }
+      SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+      SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+      JwtSecurityToken token = new JwtSecurityToken(
+          claims: claims,
+          expires: DateTime.Now.AddMinutes(expirationMinutes),
+          signingCredentials: creds
+      );
+      return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+    public static string GenerateRefreshTokenAsync(string userId)
+    {
+      var refreshToken = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
+      return refreshToken;
+    }
+    public static ClaimsPrincipal ValidateToken(string token, string secretKey)
+    {
+      JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+      byte[] key = Encoding.UTF8.GetBytes(secretKey);
+      TokenValidationParameters validationParameters = new TokenValidationParameters
+      {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+      };
+      try
+      {
+        return tokenHandler.ValidateToken(token, validationParameters, out _);
+      }
+      catch (Exception ex)
+      {
+        throw new SecurityTokenException("Invalid token", ex);
+      }
+    }
+
   }
 }
-                     
--- Zymora/Authentication/JWTService.cs
+++ Zymora/Authentication/JWTService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using System.IdentityModel.Tokens.Jwt;
@@ -11,40 +13,84 @@ using Zymora_BE.Repositories.DataContext;
 
 namespace Zymora.Authentication
 {
-    public class JWTService
+  public class JWTService
+  {
+    private readonly JwtSettings _settings;
+    private readonly IConfiguration _configuration;
+    private readonly DatabaseContext _context;
+    public JWTService(IOptions<JwtSettings> options, IConfiguration configuration, DatabaseContext context)
     {
-        private readonly JwtSettings _settings;
-        private readonly IConfiguration _configuration;
-        public JWTService(IOptions<JwtSettings> options, IConfiguration configuration)
-        {
-            _settings = options.Value;
-            _configuration = configuration;
-          }
-        public string GenerateToken(User user, List<Roles> rolesUser)
-        {
-            
-            List<Claim> claims = new List<Claim>
+      _settings = options.Value;
+      _configuration = configuration;
+      _context = context;
+    }
+    public async Task<LoginResponse> GenerateToken(User user, List<Roles> rolesUser)
+    {
+
+      List<Claim> claims = new List<Claim>
             {
                 new Claim(ClaimTypes.NameIdentifier, user.Id),
                 new Claim(ClaimTypes.Name, user.UserName ?? string.Empty),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
             };
-            foreach (var role in rolesUser)
-            {
-                claims.Add(new Claim(ClaimTypes.Role, role.Name ?? string.Empty));
-            }
-            string? secretKey = _configuration["JWT:secretKey"];
-            if (string.IsNullOrEmpty(secretKey))
-            {
-                throw new ArgumentNullException(nameof(secretKey));
-            }
-            string token = JWTHelper.GenerateToken(
-                claims: claims,
-                expirationMinutes: Convert.ToInt32(_configuration["JWT:expirationMinutes"]),
-                secretKey: secretKey
-            );
-            return token;
-        }
-        
+
+      foreach (var role in rolesUser)
+      {
+        claims.Add(new Claim(ClaimTypes.Role, role.Name ?? string.Empty));
+      }
+
+      string? secretKey = _configuration["JWT:secretKey"];
+
+      if (string.IsNullOrEmpty(secretKey))
+      {
+        throw new ArgumentNullException(nameof(secretKey));
+      }
+
+      string token = JWTHelper.GenerateToken(
+          claims: claims,
+          expirationMinutes: Convert.ToInt32(_configuration["JWT:expirationMinutes"]),
+          secretKey: secretKey
+      );
+
+      string refreshToken = JWTHelper.GenerateRefreshTokenAsync(user.Id);
+
+      await RevokeRefreshToken(refreshToken, user);
+
+      var expires = DateTime.UtcNow.AddMinutes(
+      Convert.ToDouble(_configuration["Jwt:expirationMinutes"])
+      );
+
+      return new LoginResponse { AccessToken = token, RefreshToken = refreshToken, ExpiresAt = expires };
+    }
+    
+    public async Task RevokeRefreshToken(string refreshToken, User user) // Handle adding a new refreshToken into the database and deleting old refreshToken.
+    {
+      UserToken userToken = new UserToken
+      {
+        UserId = user.Id,
+        LoginProvider = "JWT",
+        Name = "RefreshToken",
+        Value = refreshToken,
+        User = user
+      };
+      UserToken? exstingToken = _context.User_tokens.FirstOrDefault(ut => ut.Value == refreshToken && ut.LoginProvider == "JWT" && ut.Name == "RefreshToken");
+      if (exstingToken != null)
+      {
+        _context.User_tokens.Remove(exstingToken);
+      }
+      _context.User_tokens.Add(userToken);
+      await _context.SaveChangesAsync();
+    }
+    public async Task<bool> ValidateRefreshTokenAsync(string userId, string refreshToken)
+    {
+      var token = await _context.User_tokens
+          .FirstOrDefaultAsync(t =>
+              t.UserId == userId &&
+              t.LoginProvider == "JWT" &&
+              t.Name == "RefreshToken" &&
+              t.Value == refreshToken);
+
+      return token != null;
+    }
   }
 }
